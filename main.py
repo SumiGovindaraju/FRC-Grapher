0a39ceb37f4819bf4a8cacdde62b6906496edc6a
SumiGovindaraju <sumi.govindaraju20@bcp.org>
2019-07-29 20:30:39 -0400

    Update license; make more robust; cleanup; update pynetworktables

--- main.py
+++ main.py
@@ -1,44 +1,67 @@
 #!/usr/bin/env python3
 
+"""
+Websocket that forwards relevant Network Tables data to the webpage and also caches data for that instance of the page for later use.
+"""
+
 import argparse
 import datetime
 import json
 import logging
-from networktables import NetworkTables
-from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+import os
 import signal
 import sys
 from threading import Thread
-import time
 
-parser = argparse.ArgumentParser(description="FRC-Grapher, Copyright (c) 2018 Sumi Govindaraju")
-parser.add_argument("-c", "--config", action="store_true", help="use the saved configuration file")
-parser.add_argument("-i", "--ip", default="localhost", help="IP address of NetworkTables server", metavar="", type=str)
-parser.add_argument("-r", "--replay-match", default=False, help="path to JSON cache file for match replay", metavar="", type=str)
+from networktables import NetworkTables
+from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+
+__author__ = "SumiGovindaraju"
+__copyright__ = "Copyright (c) 2018-2019 Sumi Govindaraju"
+__credits__ = ["SumiGovindaraju"]
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "SumiGovindarajut"
+
+parser = argparse.ArgumentParser(
+    description="FRC-Grapher, " + __copyright__)
+parser.add_argument("-c", "--config", help="Path to saved configuration file", metavar="")
+parser.add_argument("-i", "--ip", default="localhost",
+                    help="IP address of NetworkTables server", metavar="", type=str)
+parser.add_argument("-r", "--replay-match", default=False,
+                    help="path to JSON cache file for match replay", metavar="", type=str)
 args = parser.parse_args()
+
 
 def loadConfig():
     ret_val = set()
     if args.config:
         try:
-            with open('config.json', 'r') as infile:
+            with open(args.config, 'r') as infile:
                 data = json.loads(infile.read())
                 for i in data:
                     ret_val.add(i)
         except FileNotFoundError:
             print("Configuration file not found. Using empty set instead.")
+    else:
+        print("Configuration file not specified. Using empty set instead.")
 
     return ret_val
 
+
 keys = loadConfig()
 connections = set()
+
 
 class GrapherWebSocket(WebSocket):
     def handleMessage(self):
         if (self.data.startswith("Add Dataset: ")):
             keys.add(self.data[13:])
         elif (self.data == "Save Config"):
-            with open('config.json', 'w') as outfile:
+            filename = "config.json"
+            if args.config != None:
+                filename = args.config
+            with open(filename, 'w') as outfile:
                 outfile.write(json.dumps(list(keys)))
 
     def handleConnected(self):
@@ -56,6 +79,7 @@ class GrapherWebSocket(WebSocket):
     def handleClose(self):
         connections.remove(self)
 
+
 server = SimpleWebSocketServer('localhost', 8254, GrapherWebSocket)
 
 global currentTimestamp
@@ -68,18 +92,27 @@ colors = [
     'rgb(153, 102, 255)',
     'rgb(255, 205, 86)',
     'rgb(54, 162, 235)',
-	'rgb(75, 192, 192)'
+    'rgb(75, 192, 192)'
 ]
-cacheFileName = "cache/FRC Grapher JSON Cache " + datetime.datetime.now().strftime("%Y-%m-%d %I.%M.%S %p") + ".json"
+
+if not os.path.isdir("cache/"):
+    print("not a dir")
+    os.makedirs("cache/")
+cacheFileName = "cache/FRC Grapher JSON Cache " + \
+    datetime.datetime.now().strftime("%Y-%m-%d %I.%M.%S %p") + ".json"
+
+
 def valueChanged(table, key, value, isNew):
     global currentTimestamp
     if key == 'frc-grapher-timestamp':
         currentTimestamp = value
         for connection in connections:
-            connection.sendMessage(u"" + json.dumps({'key': str(key), 'value': str(value)}))
+            connection.sendMessage(
+                u"" + json.dumps({'key': str(key), 'value': str(value)}))
     if key in keys:
         for connection in connections:
-            connection.sendMessage(u"" + json.dumps({'key': str(key), 'value': str(value)}))
+            connection.sendMessage(
+                u"" + json.dumps({'key': str(key), 'value': str(value)}))
         if not key in datasets:
             color = len(datasets) % len(colors)
             datasets[key] = {
@@ -94,7 +127,8 @@ def valueChanged(table, key, value, isNew):
             }
         else:
             if (datasets[key]["data"][len(datasets[key]["data"]) - 1]["x"] == currentTimestamp):
-                datasets[key]["data"][len(datasets[key]["data"]) - 1]["y"] = value;
+                datasets[key]["data"][len(
+                    datasets[key]["data"]) - 1]["y"] = value
             else:
                 datasets[key]["data"].append({
                     "x": currentTimestamp,
@@ -104,21 +138,26 @@ def valueChanged(table, key, value, isNew):
         with open(cacheFileName, "w") as outfile:
             json.dump(datasets, outfile)
 
+
 def connectionListener(connected, info):
     print(info, '; Connected=%s' % connected)
+
 
 if not args.replay_match:
     logging.basicConfig(level=logging.DEBUG)
 
     NetworkTables.initialize(server=args.ip)
-    NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
-    
+    NetworkTables.addConnectionListener(
+        connectionListener, immediateNotify=True)
+
     sd = NetworkTables.getTable("SmartDashboard")
     sd.addEntryListener(valueChanged)
+
 
 def close_sig_handler(signal, frame):
     server.close()
     sys.exit()
+
 
 signal.signal(signal.SIGINT, close_sig_handler)
 server.serveforever()
